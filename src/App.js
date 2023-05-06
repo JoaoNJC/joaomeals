5342d09743654a4e34a20930128246ee19fce78e
Joao Camba <joaocamba@tests-iMac.local>
2023-05-06 18:22:23 +0100

    Meals skeleton. Summary + List + Card component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from './Components/Layout/Header';
+import Meals from './Components/Meals/Meals';
 
 const App = (props) => {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   )
 }
